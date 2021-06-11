ea4221278e73f063c487e04691368718d6a8404d
Aleksandr Zavgorodnii <82917012+AleksZavg@users.noreply.github.com>
2021-06-11 18:59:48 +1000

    Немножко исправлений

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,4 +1,4 @@
-import logging, time
+import logging
 
 from aiogram import Dispatcher
 
@@ -8,8 +8,7 @@ from data.config import data
 async def on_startup_notify(dp: Dispatcher):
     for admin in data["who_need_notify"]:
         try:
-            await dp.bot.send_message(admin, f"❗️ [{time.strftime('%m/%d/%Y, %H:%M:%S')}] - Я была перезагружена!")
-            # 12/07/2019, 15:01:09 - что выдаст
+            await dp.bot.send_message(admin, f"❗️ Я была перезагружена!")
             await dp.bot.send_sticker(admin, "CAACAgIAAxkBAAECaKhgwdTtsNAlX5VkuSSA-2Gax_DGOgAC9QADAexmGk1IYGxz6uPMHwQ")
 
         except Exception as err:
